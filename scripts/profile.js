c2842025ad8bab1be142d5cd6bf58323934de5e3
Lih3006 <alineb3006@hotmail.com>
2023-02-17 16:01:10 +0100

    teste caminho relativo

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,6 +1,6 @@
-import { renderUser, renderRepositories } from "./render.js";
-import { requestRepository } from "./requests.js";
-import { standartString } from "./usefulFunctions.js";
+import { renderUser, renderRepositories } from "../scripts/render.js";
+import { requestRepository } from "../scripts/requests.js";
+import { standartString } from "../scripts/usefulFunctions.js";
 
 const user = JSON.parse(localStorage.getItem("requestUser"));
 
