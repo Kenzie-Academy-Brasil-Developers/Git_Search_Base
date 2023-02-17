2d38e201e90510ce61f0d802bbe922e70992db9d
Lih3006 <alineb3006@hotmail.com>
2023-02-17 13:50:16 +0100

    teste caminho relativo

--- scripts/requests.js
+++ scripts/requests.js
@@ -19,11 +19,11 @@ export async function requestUser(userName) {
     })
     .then((res) => {
       if (res.message === "Not Found") {
-        window.location.replace("../pages/error/index.html");
+        window.location.replace("../error/index.html");
         return res;
       } else {
         localStorage.setItem("requestUser", JSON.stringify(res));
-        window.location.replace("../pages/profile/index.html");
+        window.location.replace("../profile/index.html");
       }
     });
 
